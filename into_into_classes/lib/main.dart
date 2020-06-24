58d051652cbee70d4933e55c7274f4810f675e5f
darkjedeye <eric@kleincore.co.za>
2020-06-24 16:24:11 +0200

    next chapter - inheritence

--- into_into_classes/lib/main.dart
+++ into_into_classes/lib/main.dart
@@ -9,6 +9,7 @@ class Microphone{
   // bool power;
 //Below is what are known as Methods! Methods can be void or types, just like Functions!
 // It seems Methods should always be above Constructors
+  //methods
   void turnOn() {
     print("$name is on!");
   }
@@ -45,7 +46,7 @@ class Microphone{
 
 
 //  Microphone(String name, String color, int model) {
-//// when using this.whatever = whateverelse, thi. refers to the class instance.
+//// when using this.whatever = whateverelse, this . refers to the class instance.
 //// and the class instance is always first!
 //    this.name = name;
 //    this.color = color;
@@ -53,6 +54,10 @@ class Microphone{
 //  }
 
 //Microphone(this.name, this.color, this.model);
+//getters and setters - not common practice, but available if needs be
+
+String get getName => name; //getter
+set setName(String value) => name = value; //setter
 }
 
 class Heilo{
@@ -85,33 +90,48 @@ class Heilo{
 main(List<String> arguments) {
 
   var mic = new Microphone("Blue Yetti", "Silver-Grey", 1345 ); //We are Creating the actual object of type mic
-  var mic2 = new Microphone.initializer();//Using the initialized named constructo
-//  mic.name = "Blue Yetti"; these types of commands will also overide the value.
-//  mic.color = "Silver-Grey";
-//  mic.model = 1345;
-//  mic.power = true;
+//  mic.setName = "";
+  print(mic.getName);
+  mic.setName = "The name is now fuck off";
+  print(mic.name);
 
-  print(mic2.model);
-  print(mic2.name);
 
-  print("This is ${mic.name} with a color of ${mic.color} and model number of ${mic.model}"); //. (dot) is the access operator
-  mic.turnOn();
-  mic.setVolume();
-  mic.turnOff();
 
-  print(mic.isOn());
-  print(mic.modelNumber());
-  print(" ");
-  var Helli = new Heilo("Bell", "405", "Mavrick");
-  var Helli2 = new Heilo.Blackhawk();
-  print("There is a chopper on the runway, ${Helli.hname} ${Helli.hmodel}, with"
-      " the callsign ${Helli.callsign}, that needs to taxi for takeoff!");
 
-  print(".........");
-  print(" ");
-  Helli.otaReply();
 
-  print(Helli2.hname);
-  print(Helli2.hmodel);
-  print(Helli2.callsign);
+
+
+
+
+
+
+//  var mic2 = new Microphone.initializer();//Using the initialized named constructor
+////  mic.name = "Blue Yetti"; these types of commands will also overide the value.
+////  mic.color = "Silver-Grey";
+////  mic.model = 1345;
+////  mic.power = true;
+//
+//  print(mic2.model);
+//  print(mic2.name);
+//
+//  print("This is ${mic.name} with a color of ${mic.color} and model number of ${mic.model}"); //. (dot) is the access operator
+//  mic.turnOn();
+//  mic.setVolume();
+//  mic.turnOff();
+//
+//  print(mic.isOn());
+//  print(mic.modelNumber());
+//  print(" ");
+//  var Helli = new Heilo("Bell", "405", "Mavrick");
+//  var Helli2 = new Heilo.Blackhawk();
+//  print("There is a chopper on the runway, ${Helli.hname} ${Helli.hmodel}, with"
+//      " the callsign ${Helli.callsign}, that needs to taxi for takeoff!");
+//
+//  print(".........");
+//  print(" ");
+//  Helli.otaReply();
+//
+//  print(Helli2.hname);
+//  print(Helli2.hmodel);
+//  print(Helli2.callsign);
 }
