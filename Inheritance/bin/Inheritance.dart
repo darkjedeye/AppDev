1b6e827036872203c145347e712e68c8c5092e55
darkjedeye <eric@kleincore.co.za>
2020-06-24 18:51:53 +0200

    adding in variables and testing limitations

--- Inheritance/bin/Inheritance.dart
+++ Inheritance/bin/Inheritance.dart
@@ -1,8 +1,10 @@
-
-
 class  Person{
   String name, lastName, nationality;
   int age;
+  String hairColor, eyeColor, breastSize;
+  double heighMetric;
+  num waistSize;
+  bool facialHair, Tattoos, Pregnant, programmer;
 
 
 
@@ -10,21 +12,36 @@ class  Person{
 
 
 class Bonni extends Person {
-  String hairColor, eyeColor, breastSize;
-  double heighMetric, waistSize;
-  bool facialHair, Tattoos;
-  void showName() {
-    print(this.name);
 
-  }
+
   void assets() {
-    print(this.lastName + " has " + this.hairColor + " hair, with " + this.eyeColor + " eyes " + this.breastSize +" breats and a height of "
+    print(this.name + " " + this.lastName + " has " + this.hairColor + " hair, with " + this.eyeColor + " eyes " + this.breastSize +" breats and a height of "
         + this.heighMetric.toString() +"m and a waist size of " + this.waistSize.toString() + "cm. Does she have facial hair? " + this.facialHair.toString()
-         + ". Does she have tatoos? " + this.Tattoos.toString() + ".");
+        + ". Does she have tatoos? " + this.Tattoos.toString() + ".");
 
   }
 }
 
+class Cherise extends Person {
+
+  void myLove() {
+    print(this.name + " " + this.lastName + " has " + this.hairColor +
+        " hair with " + this.eyeColor + " eyes and " + breastSize
+        + " Breasts, and a height of " + heighMetric.toString() +
+        "m and a waist size of " + this.waistSize.toString() + "cm"
+        "and is pregnant? " + this.Pregnant.toString());
+  }
+}
+  class Eric extends Person {
+
+  void completely() {
+  print(this.name + " " + this.lastName + " has " + this.hairColor + " hair with " + this.eyeColor + " eyes and " +breastSize
+  + " Breasts, and a height of " + heighMetric.toString() + "m and a waist size of " + this.waistSize.toString() + "cm"
+  " and is a programmer? " + this.programmer.toString());
+  }
+
+
+}
 
 void main(List<String> arguments) {
   var bonni = new Bonni();
@@ -38,9 +55,37 @@ void main(List<String> arguments) {
   bonni.facialHair = false;
   bonni.Tattoos = true;
 
+  bonni.assets();
+print(" ");
 
+ var Chez = new Cherise();
+ Chez.name = "Cherise";
+ Chez.lastName = "Klein";
+ Chez.breastSize = "36C";
+ Chez.eyeColor = "brown";
+ Chez.hairColor = "Brunnette";
+ Chez.heighMetric = 1.75;
+ Chez.waistSize = 40;
+ Chez.facialHair = false;
+ Chez.Tattoos = true;
+ Chez.Pregnant = true;
 
- bonni.showName();
- bonni.assets();
+ Chez.myLove();
+
+ print(" ");
+
+ var Me = new Eric();
+  Me.name = "Eric";
+  Me.lastName = "Klein";
+  Me.breastSize = "0";
+  Me.eyeColor = "Blue";
+  Me.hairColor = "Brunnette";
+  Me.heighMetric = 1.85;
+  Me.waistSize = 32;
+  Me.facialHair = true;
+  Me.Tattoos = true;
+  Me.programmer = true;
+
+  Me.completely();
   }
 
