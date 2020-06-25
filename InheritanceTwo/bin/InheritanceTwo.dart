ef3f32507c1c7a400db3072ae03aee10ea8ea690
darkjedeye <eric@kleincore.co.za>
2020-06-25 08:48:38 +0200

    New Chapter interfaces and abstract classes

--- InheritanceTwo/bin/InheritanceTwo.dart
+++ InheritanceTwo/bin/InheritanceTwo.dart
@@ -1,5 +1,37 @@
 
 
-void main(List<String> arguments) {
+class Location {
+  num lat, long; //instance variables or member fields
+
+Location(this.lat, this.long);
+
+//named constructor
+Location.create(this.lat, this.long);
+
+}
+
+class elevatedLocation extends Location {
+  num elevation;
+  elevatedLocation(num lat, num long, this.elevation) : super(lat, long); // call location constructor
+
+@override
+  String toString() {
+ var result = "$lat, $long, $elevation";
+    return result;
+  }
+}
+
+
+main(List<String> arguments) {
+
+  String name = "Bonnie";
+
+
+
+
+  var elevated = elevatedLocation(23.89, -234.98, 90);
+  print(elevated);
+//
+//  print("location=${elevated.lat}, ${elevated.long}, ${elevated.elevation}");
 
 }
